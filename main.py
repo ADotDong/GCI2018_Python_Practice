3d1097a531b6e107742b7c04229c55720eff1c81
ADotDong <30413567+ADotDong@users.noreply.github.com>
2018-11-01 20:25:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+for i in range(10):
+  print("GCI is great")
+print("What is your name?")
+answer = input()
+backwardAnswer = answer[::-1]
+print("Hello, " + answer + ". Pleased to meet you! Did you know that your name backwards is " + backwardAnswer)
